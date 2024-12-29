6d1cf72b947006efc3696350ede7395bfd06879c
ephrim-s <123345937+ephrim-s@users.noreply.github.com>
2024-12-29 12:00:43 -0500

    deleted mongodb urls

--- BookStore-api/database/db.js
+++ BookStore-api/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDB = async()=>{
   try{
-    await mongoose.connect('mongodb+srv://ephrims5:BGgtECVWIk6ZnyiH@cluster0.kfdwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    await mongoose.connect();
     console.log('mongodb is connected successfully');
 
   }catch(error){
